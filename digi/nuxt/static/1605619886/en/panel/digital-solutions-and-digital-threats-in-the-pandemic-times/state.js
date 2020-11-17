2a7ff91a89919989273e3957d754da6711598257
Adam Benda <adam@adasek.cz>
2020-11-17 14:32:58 +0100

    add arrows

---
+++ digi/nuxt/static/1605619886/en/panel/digital-solutions-and-digital-threats-in-the-pandemic-times/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1605619886",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fdigital-solutions-and-digital-threats-in-the-pandemic-times\u002F",config:{content:{dbHash:"c9362b4c"}}};
