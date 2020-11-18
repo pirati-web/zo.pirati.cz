a54b8b548126b43e48c68a03a37039aa309b5270
Adam Benda <adam@adasek.cz>
2020-11-18 13:51:14 +0100

    digiweb build v5: remove Seth Stephens-Davidowitz, webp images

---
+++ digi/nuxt/static/1605703586/en/panel/digital-solutions-and-digital-threats-in-the-pandemic-times/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1605703586",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fdigital-solutions-and-digital-threats-in-the-pandemic-times\u002F",config:{content:{dbHash:"3d41b8c0"}}};
