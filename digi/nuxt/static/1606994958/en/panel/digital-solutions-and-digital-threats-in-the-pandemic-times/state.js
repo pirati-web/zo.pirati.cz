dcafa8b2ffb45445198865727e05f7b32cb32a2e
Adam Benda <adam@adasek.cz>
2020-12-05 00:33:37 +0100

    Digiweb v12: video links

--- digi/nuxt/static/1606994958/en/panel/digital-solutions-and-digital-threats-in-the-pandemic-times/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fnuxt\u002Fstatic\u002F1606994958",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fdigital-solutions-and-digital-threats-in-the-pandemic-times\u002F",config:{content:{dbHash:"755adda8"}}};
