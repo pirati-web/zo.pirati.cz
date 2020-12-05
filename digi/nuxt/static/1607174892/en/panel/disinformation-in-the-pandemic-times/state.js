3c466f093c882a105f732454e8242d731b68ec54
Adam Benda <adam@adasek.cz>
2020-12-05 14:32:51 +0100

    digiweb v15: add video playlist link

---
+++ digi/nuxt/static/1607174892/en/panel/disinformation-in-the-pandemic-times/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fdigi\u002Fnuxt\u002Fstatic\u002F1607174892",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fdisinformation-in-the-pandemic-times\u002F",config:{content:{dbHash:"d2c6cfdd"}}};
