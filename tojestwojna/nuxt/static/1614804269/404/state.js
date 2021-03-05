129146e8f4177ef15627ff6e4d009fabc74ab117
Adam Benda <adam@adasek.cz>
2021-03-05 08:45:31 +0100

    tojestwojna updated conclusion text

--- tojestwojna/nuxt/static/1614804269/404/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftojestwojna\u002Fnuxt\u002Fstatic\u002F1614804269",layout:"default",error:a,serverRendered:true,routePath:"\u002F404",config:{app:{basePath:"\u002Ftojestwojna\u002F",assetsPath:"\u002Ftojestwojna\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"0bc68bd0"}}}}(null));
