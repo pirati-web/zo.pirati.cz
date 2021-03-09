b5edfacb076698543a40ed337673290a72613d19
Adam Benda <adam@adasek.cz>
2021-03-09 23:25:45 +0100

    tojestwojna: fix typo

---
+++ tojestwojna/nuxt/static/1615328436/404/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftojestwojna\u002Fnuxt\u002Fstatic\u002F1615328436",layout:"default",error:a,serverRendered:true,routePath:"\u002F404",config:{app:{basePath:"\u002Ftojestwojna\u002F",assetsPath:"\u002Ftojestwojna\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"abae0c9a"}}}}(null));
