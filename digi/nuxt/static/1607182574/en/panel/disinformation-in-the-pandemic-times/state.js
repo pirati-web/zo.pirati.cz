12486815b60cdfb6c67e727c9d9e7f50309893b2
Adam Benda <adam@adasek.cz>
2021-03-30 16:45:21 +0200

    Add video recordings

--- digi/nuxt/static/1607182574/en/panel/disinformation-in-the-pandemic-times/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fdigi\u002Fnuxt\u002Fstatic\u002F1607182574",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fdisinformation-in-the-pandemic-times\u002F",config:{content:{dbHash:"5a531785"}}};
--- digi/nuxt/static/1607182574/en/panel/the-future-of-european-digital-policy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fdigi\u002Fnuxt\u002Fstatic\u002F1607182574",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fthe-future-of-european-digital-policy\u002F",config:{content:{dbHash:"5a531785"}}};
