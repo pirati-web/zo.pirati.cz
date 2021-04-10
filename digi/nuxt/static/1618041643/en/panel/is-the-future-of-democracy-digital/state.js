0dfcbb585d80fbd6aba1b6a1779cdd89a432066f
Adam Benda <adam@adasek.cz>
2021-04-10 10:06:33 +0200

    Going digital: subtitles

---
+++ digi/nuxt/static/1618041643/en/panel/is-the-future-of-democracy-digital/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdigi\u002Fnuxt\u002Fstatic\u002F1618041643",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fpanel\u002Fis-the-future-of-democracy-digital\u002F",config:{_app:{basePath:"\u002Fdigi\u002F",assetsPath:"\u002Fdigi\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"0e3b996a"}}}}(null));
