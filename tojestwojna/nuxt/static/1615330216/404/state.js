77a9da7918cc975e629d9ccaa9ef826eefe02b19
Adam Benda <adam@adasek.cz>
2022-03-21 22:35:45 +0100

    Tojestwojna:Fix typo, upgrade packages

--- tojestwojna/nuxt/static/1615330216/404/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftojestwojna\u002Fnuxt\u002Fstatic\u002F1615330216",layout:"default",error:a,serverRendered:true,routePath:"\u002F404",config:{app:{basePath:"\u002Ftojestwojna\u002F",assetsPath:"\u002Ftojestwojna\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"3c3784d6"}}}}(null));
---
+++ tojestwojna/nuxt/static/1647898380/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftojestwojna\u002Fnuxt\u002Fstatic\u002F1647898380",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftojestwojna\u002F",assetsPath:"\u002Ftojestwojna\u002Fnuxt\u002F",cdnURL:a},content:{dbHash:"295e4932"}}}}(null));
